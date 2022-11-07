522a8abb7a14440554a7e2e57d8ddedba1d71877
Danny-Lenko <devdanny.14@gmail.com>
2022-11-07 14:36:07 +0200

    traits section is ready

--- src/modules/Home/HomeTraits/SingleTrait.tsx
+++ src/modules/Home/HomeTraits/SingleTrait.tsx
@@ -1,12 +1,30 @@
 import Grid from '@mui/material/Unstable_Grid2';
 import Box from '@mui/material/Box'
 import Typography from '@mui/material/Typography'
+import { Theme } from "@mui/system"
+import { calcSpacing } from "../../../library/utilities/utils"
+import { SECTIONMARGINBOTTOM } from "../../../library/common/constants/contstants"
 
 const SingleTrait = ({ propsObj }: any) => {
    const { mainImage, bgImage, title, text } = propsObj
 
+   const containerStyles = {
+      mb: {
+         xs: 10,
+         sm: 4,
+         md: (theme:Theme) => calcSpacing(theme, SECTIONMARGINBOTTOM),
+      },
+      textAlign: { xs: 'center', sm: 'start', md: 'center' },
+      justifyContent: 'center',
+      alignItems: 'flexStart',
+      '& .trait-grid-item2': {
+         minHeight: '100%',
+         pl: { xs: 0, sm: 4, md: 0 }
+      }
+   }
+
    return (
-      <Grid container xs={12} md={4} textAlign='center' justifyContent='center'>
+      <Grid container xs={12} md={4} sx={containerStyles}>
          <Grid xs={10} sm={4} md={8} sx={bgImage}>
             <Box
                component='img'
@@ -14,8 +32,8 @@ const SingleTrait = ({ propsObj }: any) => {
                src={mainImage}
             ></Box>
          </Grid>
-         <Grid xs={12} sm={8} md={12}>
-            <Typography textTransform='uppercase' variant='h3'>{title}</Typography>
+         <Grid xs={12} sm={8} md={12} className='trait-grid-item2' >
+            <Typography mt={6} mb={4} textTransform='uppercase' variant='h3'>{title}</Typography>
             <Typography>{text}</Typography>
          </Grid>
       </Grid>
