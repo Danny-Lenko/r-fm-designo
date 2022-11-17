e55694afbdadfd9d5a15e2da9906d645c38f13b2
Danny-Lenko <devdanny.14@gmail.com>
2022-11-17 18:48:30 +0200

    refactored home traits

--- src/modules/Home/HomeTraits/HomeTraits.tsx
+++ src/modules/Home/HomeTraits/HomeTraits.tsx
@@ -1,19 +1,16 @@
 import Grid from '@mui/material/Unstable_Grid2';
 import Box from '@mui/material/Box'
-import ImagePassionate from '../../../resources/assets/home/desktop/illustration-passionate.svg'
-import ImageResourceful from '../../../resources/assets/home/desktop/illustration-resourceful.svg'
-import ImageFriendly from '../../../resources/assets/home/desktop/illustration-friendly.svg'
-import BgPassionate from '../../../resources/assets/home/desktop/bg-passionate.png'
-import BgResourceful from '../../../resources/assets/home/desktop/bg-resourceful.png'
-import BgFriendly from '../../../resources/assets/home/desktop/bg-friendly.png'
+import ImagePassionate from '../../../resources/assets/home/desktop/img-group-passionate.svg'
+import ImageResourceful from '../../../resources/assets/home/desktop/img-group-resourceful.svg'
+import ImageFriendly from '../../../resources/assets/home/desktop/img-group-friendly.svg'
 import SingleTrait from './SingleTrait';
+import { SECTIONMARGINBOTTOM } from '../../../library/common/constants/constants';
 
 const HomeTraits = () => {
 
    class TraitProps {
       constructor(
          readonly mainImage: string,
-         readonly bgImage: Record<string, string>,
          readonly title: string,
          readonly text: string
       ) { }
@@ -21,33 +18,24 @@ const HomeTraits = () => {
 
    const passionateTraitProps = new TraitProps(
       ImagePassionate,
-      assembleBgImg(BgPassionate),
       'Passionate',
       'Each project starts with an in-depth brand research to ensure we only create products that serve a purpose. We merge art, design, and technology into exciting new solutions.'
    )
 
    const resourcefulTraitProps = new TraitProps(
       ImageResourceful,
-      assembleBgImg(BgResourceful),
       'Resourceful',
       "Everything that we do has a strategic purpose. We use an agile approach in all of our projects and value customer collaboration. It guarantees superior results that fulfill our clients' needs."
    )
 
    const friendlyTraitProps = new TraitProps(
       ImageFriendly,
-      assembleBgImg(BgFriendly),
       'Friendly',
       "We are a group of enthusiastic folks who know how to put people first. Our success depends on our customers, and we strive to give them the best experience a company can provide."
    )
 
-   function assembleBgImg(bgImg: any) {
-      return {
-         background: `url(${bgImg}) 50% 50%/202px 202px no-repeat`
-      }
-   }
-
    return (
-      <Box sx={{ flexGrow: 1 }}>
+      <Box sx={{ flexGrow: 1, mb:  SECTIONMARGINBOTTOM / 8}}>
          <Grid container spacing={2}>
             {/* passionate */}
             <SingleTrait propsObj={passionateTraitProps} />
--- src/modules/Home/HomeTraits/SingleTrait.tsx
+++ src/modules/Home/HomeTraits/SingleTrait.tsx
@@ -3,34 +3,39 @@ import Box from '@mui/material/Box'
 import Typography from '@mui/material/Typography'
 
 const SingleTrait = ({ propsObj }: any) => {
-   const { mainImage, bgImage, title, text } = propsObj
+   const { mainImage, title, text } = propsObj
 
    const containerStyles = {
       mb: {
          xs: 10,
-         sm: 108 / 8,
-         md: 20
+         sm: 4
       },
       textAlign: { xs: 'center', sm: 'start', md: 'center' },
       justifyContent: 'center',
-      alignItems: 'flexStart',
-      '& .trait-grid-item2': {
-         minHeight: '100%',     // may possibly cause problems
-         pl: { xs: 0, sm: 4, md: 0 }
+      '& .text-box': {
+         display: 'flex',
+         flexDirection: 'column',
+         justifyContent: 'center',
+         pl: {sm: 4, md: 0},
+         '& .MuiTypography-h3': {
+            textTransform: 'uppercase',
+            mt: {xs: 5, sm: 0, md: 5},
+            mb: {xs: 4, sm: 2, md: 4}
+         }
       }
    }
 
    return (
       <Grid container xs={12} md={4} sx={containerStyles}>
-         <Grid xs={10} sm={4} md={8} sx={bgImage}>
+         <Grid xs={10} sm={4} md={8} className='img-box'>
             <Box
                component='img'
                alt='friendly'
                src={mainImage}
             ></Box>
          </Grid>
-         <Grid xs={12} sm={8} md={12} className='trait-grid-item2' >
-            <Typography mt={6} mb={4} textTransform='uppercase' variant='h3'>{title}</Typography>
+         <Grid xs={12} sm={8} md={12} className='text-box' >
+            <Typography variant='h3'>{title}</Typography>
             <Typography>{text}</Typography>
          </Grid>
       </Grid>
